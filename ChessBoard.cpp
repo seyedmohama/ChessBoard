a1d2e0fef8789f0e543c05d173be5ba8a3c00302
mmotahari5 <mmotahari5@gmail.com>
2021-08-04 17:51:48 +0430

    Debug ChessBoard.cpp

--- ChessBoard.cpp
+++ ChessBoard.cpp
@@ -5,7 +5,6 @@ void ChessBoard::initBoard()
   Board = new Cell*[8];
   for (int i = 0; i < 8; i++)
     Board[i] = new Cell[8];
-	
 
   //Setting white pawns
   Board[0][0].SetPawn(new Rook(0, 0, ChessColor::Black));
@@ -91,8 +90,8 @@ bool ChessBoard::IsChecked(ChessColor color)
       if (!Board[i][j].IsEmpty()
       && Board[i][j].ptr->Color == targetcolor)
       {
-        auto moves = Board[i][j].ptr->GetMovements();
-        for (auto k = moves.begin(); i != moves.end(); i++)
+        auto moves = Board[i][j].ptr->GetMovements(Board);
+        for (auto k = moves.begin(); k != moves.end(); k++)
         {
           if (k->first == king.first && k->second == king.second)
             return true;
@@ -108,7 +107,7 @@ bool ChessBoard::IsMated(ChessColor color)
 {
   auto king = FindKing(color);
 
-  auto kingmoves = Board[king.first][king.second].ptr->GetMovements();
+  auto kingmoves = Board[king.first][king.second].ptr->GetMovements(Board);
 
 
 
@@ -121,10 +120,18 @@ bool ChessBoard::IsMated(ChessColor color)
       if (!Board[i][j].IsEmpty()
       && Board[i][j].ptr->Color == targetcolor)
       {
-        auto moves = Board[i][j].ptr->GetMovements();
-        for (auto k = moves.begin(); i != moves.end(); i++)
+        auto moves = Board[i][j].ptr->GetMovements(Board);
+        for (auto k = moves.begin(); k != moves.end(); k++)
         {
-          auto it = kingmoves.find({k->first, k->second});
+          auto it = kingmoves.end();
+          for (auto f = kingmoves.begin(); f != kingmoves.end(); f++)
+          {
+            if (k->first == f->first && k->second == f->second)
+            {
+              it = f;
+            }
+          }
+
           if (it != kingmoves.end())
             kingmoves.erase(it);
         }
