b448a84de21d38bc27732874b6d4392a5c41df2a
mmotahari5 <mm.motahari.2001@gmail.com>
2021-08-12 14:05:59 +0430

    add attack movements to King

--- src/King.cpp
+++ src/King.cpp
@@ -16,15 +16,27 @@ std::vector<std::pair<int, int>> King::GetMovements(Cell** Board)
 {
   std::vector<std::pair<int, int>> movements;
 
+  if (X > 0 && !Board[X - 1][Y].IsEmpty() && (Board[X - 1][Y].ptr-> Color != Board[X][Y].ptr-> Color)){
+    movements.push_back({ X - 1, Y });
+	}
   if (X > 0 && Board[X - 1][Y].IsEmpty())
     movements.push_back({ X - 1, Y });
 
+  if (X < 7 && !Board[X + 1][Y].IsEmpty() && (Board[X + 1][Y].ptr-> Color != Board[X][Y].ptr-> Color)){
+    movements.push_back({ X + 1, Y });
+	}
   if (X < 7 && Board[X + 1][Y].IsEmpty())
     movements.push_back({ X + 1, Y });
 
+  if (Y > 0 && !Board[X][Y - 1].IsEmpty() && (Board[X][Y - 1].ptr-> Color != Board[X][Y].ptr-> Color)){
+    movements.push_back({ X, Y - 1 });
+	}
   if (Y > 0 && Board[X][Y - 1].IsEmpty())
     movements.push_back({ X, Y - 1 });
 
+  if (Y < 7 && !Board[X][Y + 1].IsEmpty() && (Board[X][Y + 1].ptr-> Color != Board[X][Y].ptr-> Color)){
+    movements.push_back({ X, Y + 1});
+	}
   if (Y < 7 && Board[X][Y + 1].IsEmpty())
     movements.push_back({ X, Y + 1 });
 
