6633b80b250cc0be2230e3e5c9bdf2f85c7af0c6
mmotahari5 <mm.motahari.2001@gmail.com>
2021-08-12 12:17:25 +0430

    debug Pawn movements

--- src/Pawn.cpp
+++ src/Pawn.cpp
@@ -17,7 +17,9 @@ std::vector<std::pair<int, int>> Pawn::GetMovements(Cell** Board)
   std::vector<std::pair<int, int>> movements;
   if (Color == PlayersColor::Black && Y != 0)
   {
-    movements.push_back({X, Y - 1});
+		if( Board[X][Y - 1].IsEmpty()){
+    	movements.push_back({X, Y - 1});
+		}
 
     if (X != 0 && !Board[X - 1][Y - 1].IsEmpty() && Board[X - 1][Y - 1].ptr->Color != Color){
       movements.push_back({X - 1, Y - 1});
@@ -29,7 +31,9 @@ std::vector<std::pair<int, int>> Pawn::GetMovements(Cell** Board)
   }
   else if (Color == PlayersColor::White && Y != 7)
   {
-    movements.push_back({X, Y + 1});
+		if( Board[X][Y + 1].IsEmpty()){
+    	movements.push_back({X, Y + 1});
+		}
 
     if (X != 0 && !Board[X - 1][Y + 1].IsEmpty() && Board[X - 1][Y + 1].ptr->Color != Color){
       movements.push_back({X - 1, Y + 1});
@@ -42,11 +46,11 @@ std::vector<std::pair<int, int>> Pawn::GetMovements(Cell** Board)
 
   if (FirstMove)
   {
-    if (Color == PlayersColor::Black && Y != 7)
+    if (Color == PlayersColor::Black && Y != 7 && Board[X][Y - 2].IsEmpty())
     {
       movements.push_back({X, Y - 2});
     }
-    else if (Color == PlayersColor::White && Y != 0)
+    else if (Color == PlayersColor::White && Y != 0 && Board[X][Y + 2].IsEmpty())
     {
       movements.push_back({X, Y + 2});
     }
